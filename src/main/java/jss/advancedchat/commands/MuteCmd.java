af6c27aa6be633d77fb715f03481ed399e4f2fd5
SrJss <Srjss@users.noreply.github.com>
2020-11-18 01:01:20 -0300

    fix commands and file

--- src/main/java/jss/advancedchat/commands/MuteCmd.java
+++ src/main/java/jss/advancedchat/commands/MuteCmd.java
@@ -8,23 +8,22 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 
 import jss.advancedchat.AdvancedChat;
-import jss.advancedchat.utils.EventsUtils;
 import jss.advancedchat.utils.Utils;
 
 public class MuteCmd implements CommandExecutor {
 
 	private AdvancedChat plugin;
-	private EventsUtils eventsUtils = new EventsUtils(plugin);
 
 	public MuteCmd(AdvancedChat plugin) {
 		this.plugin = plugin;
 		plugin.getCommand("Mute").setExecutor(this);
 	}
 
+	@SuppressWarnings("null")
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		Player j = (Player) sender;
 		if (sender instanceof Player) {
 			FileConfiguration config = plugin.getConfigfile().getConfig();
-			Player j = (Player) sender;
 			Player p = Bukkit.getPlayer(args[0]);
 			if (p == null) {
 				Utils.sendColorMessage(j, config.getString("AdvancedChat.No-Online-Player").replace("<name>", p.getName()));
@@ -36,7 +35,7 @@ public class MuteCmd implements CommandExecutor {
 			}
 
 		} else {
-
+			Utils.sendColorMessage(j, "Test Use /mute <player>");
 		}
 		return false;
 	}
