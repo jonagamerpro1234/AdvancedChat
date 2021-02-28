5373cf4b9a3e3d542274f8be1ff03d6f89ea146c
SrJss <Srjss@users.noreply.github.com>
2021-02-27 23:15:38 -0300

    snapshot 1.6.0-1C

---
+++ src/jss/advancedchat/config/ConfigManager.java
@@ -0,0 +1,51 @@
+package jss.advancedchat.config;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.yaml.snakeyaml.Yaml;
+
+import jss.advancedchat.AdvancedChat;
+import jss.advancedchat.config.Files.FileName;
+import jss.advancedchat.utils.FileManager;
+import jss.advancedchat.utils.interfaces.FileHelper;
+
+public class ConfigManager extends FileManager{
+	
+	private AdvancedChat plugin;
+	private File file;
+	private String[] folderNames = new String[]{"Data","Gui","Log"};
+	private HashMap<String, YamlConfiguration> configs;
+	private Files files = new Files();
+	private boolean removeExist;
+	
+	public ConfigManager(AdvancedChat plugin) {
+		super(plugin);
+		this.plugin = plugin;
+	}
+	
+	/*public void load() {
+		this.removeExist = this.configs != null;
+		if(!removeExist) {
+			this.configs = new HashMap<>();
+		}
+		
+		for(int i = 0; i < files.getName(FileName.valueOf(configs.))) {
+			
+		}
+	}*/
+	
+	public YamlConfiguration getConfig(FileName name) {
+		return (YamlConfiguration) this.configs.get(files.getName(name));
+	}
+	
+
+	
+}
---
+++ src/jss/advancedchat/config/Files.java
@@ -0,0 +1,45 @@
+package jss.advancedchat.config;
+
+public class Files {
+	
+	//file names list
+	
+	public String getName(FileName name) {
+		switch (name) {
+		case CONFIG:
+			return "config.yml";
+		case BUNGEECONFIG:
+			return "bungee-config.yml";
+		case CHATLOG:
+			return "chat.yml";			
+		case CHATLOGDATA:
+			return "chat-log.data";
+		case COMMANDLOG:
+			return "command.yml";
+		case COMMANDLOGDATA:
+			return "command-log.data";
+		case COLORGUI:
+			return "color-gui.yml";
+		case PLAYERGUI:
+			return "player-gui.yml";
+		case PLAYERDATA:
+			return "players.data";
+		}
+		return null;
+	}
+	
+	
+	public enum FileName {
+		CONFIG,
+		BUNGEECONFIG,
+		CHATLOG,
+		CHATLOGDATA,
+		COMMANDLOG,
+		COMMANDLOGDATA,
+		COLORGUI,
+		PLAYERGUI,
+		PLAYERDATA;
+	}
+	
+	
+}
