82d8889fab5a47afca8b4d9fe3d9e72294b9c689
SrJss <Srjss@users.noreply.github.com>
2021-02-27 23:56:59 -0300

    snapshot 1.6.0-4C, config file

--- src/jss/advancedchat/bungee/AdvancedChat.java
+++ src/jss/advancedchat/bungee/AdvancedChat.java
@@ -1,5 +1,8 @@
 package jss.advancedchat.bungee;
 
+import java.io.File;
+import java.io.IOException;
+
 import jss.advancedchat.bungee.commands.AdvancedChatCmd;
 import jss.advancedchat.bungee.utils.Utils;
 import net.md_5.bungee.api.ProxyServer;
@@ -7,23 +10,62 @@ import net.md_5.bungee.api.plugin.Command;
 import net.md_5.bungee.api.plugin.Plugin;
 import net.md_5.bungee.api.plugin.PluginDescription;
 import net.md_5.bungee.api.plugin.PluginManager;
+import net.md_5.bungee.config.Configuration;
+import net.md_5.bungee.config.ConfigurationProvider;
+import net.md_5.bungee.config.YamlConfiguration;
+
 
 public class AdvancedChat extends Plugin {
 
     private PluginDescription jss = getDescription();
     public String name = this.jss.getName();
     public String version = this.jss.getVersion();
+    private File file;
+    private Configuration config;
 
     public void onEnable() {
         Utils.setBungeeEnabled(version);
+        setupConfigFile();
         setupCommands();
+        setupEvents();
+        
 
     }
 
     public void onDisable() {
-
+    	Utils.setBungeeDisabled(version);
     }
-
+    
+    public void setupConfigFile() {
+    	this.file = new File(Utils.getDataFolder(), "bungee-config.yml");
+    	try {
+        	if(!this.file.exists()) {
+        		this.file.createNewFile();
+        	}
+        	config = load(this.file);
+        	Utils.sendBungeeColorMessage(ProxyServer.getInstance().getConsole(), Utils.getPrefix() + "&aLoad Config File");
+        	save(this.config, this.file);
+    	}catch(IOException e) {
+    		e.printStackTrace();
+    	}
+    		
+    }
+    
+    public Configuration load(File file) throws IOException {
+    	return ConfigurationProvider.getProvider(YamlConfiguration.class).load(file);
+    }
+    public void save(Configuration config, File file) {
+    	try {
+			ConfigurationProvider.getProvider(YamlConfiguration.class).save(config, file);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    }
+    
+    public Configuration getConfig() {
+    	return this.config;
+    }
+    
     public void setupCommands() {
         registerCommand(new AdvancedChatCmd());
     }
