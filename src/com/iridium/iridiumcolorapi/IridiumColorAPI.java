73d4737d5bcbeec2a49a4754cce7e2e40668beb3
SrJss <Srjss@users.noreply.github.com>
2021-03-15 17:29:04 -0300

    snapshot 1.6.0-13C

--- src/com/iridium/iridiumcolorapi/IridiumColorAPI.java
+++ src/com/iridium/iridiumcolorapi/IridiumColorAPI.java
@@ -17,6 +17,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+@SuppressWarnings("unused")
 public class IridiumColorAPI {
 
     /**
--- src/jss/advancedchat/module/ModuleManager.java
+++ src/jss/advancedchat/module/ModuleManager.java
@@ -5,10 +5,12 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.jar.JarFile;
 
+@SuppressWarnings("unused")
 public class ModuleManager extends ModuleDescription {
 	
 
 	
+	@SuppressWarnings("resource")
 	public void getModule(File file) {
 		try {
 			JarFile jarFile = new JarFile(file);
