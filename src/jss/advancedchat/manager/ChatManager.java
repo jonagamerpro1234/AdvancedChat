a0906f9786b805797ad1745a1add313f621301d8
SrJss <Srjss@users.noreply.github.com>
2021-04-30 13:13:36 -0400

    v1.6.0-beta 4, bug fix

--- src/jss/advancedchat/manager/ChatManager.java
+++ src/jss/advancedchat/manager/ChatManager.java
@@ -9,7 +9,6 @@ public class ChatManager {
     private Player player;
     private String date;
     private String time;
-    
 
     public ChatManager(String date, String time, String message, Player player) {
         this.message = message;
--- src/jss/advancedchat/module/Module.java
+++
@@ -1,9 +0,0 @@
-package jss.advancedchat.module;
-
-public abstract class Module extends ModuleManager {
-	
-	public void onEnable() {}
-	
-	public void onDisable() {}
-	
-}
