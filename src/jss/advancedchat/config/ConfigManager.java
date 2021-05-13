611705425b6397d4fffa6b6d93b5596a3b736e89
SrJss <Srjss@users.noreply.github.com>
2021-05-12 21:21:18 -0400

    v1.6.0-beta 5

--- src/jss/advancedchat/config/ConfigManager.java
+++ src/jss/advancedchat/config/ConfigManager.java
@@ -2,39 +2,94 @@ package jss.advancedchat.config;
 
 import java.io.File;
 import java.util.HashMap;
+
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import jss.advancedchat.AdvancedChat;
-import jss.advancedchat.config.Files.FileName;
-@SuppressWarnings("unused")
+import jss.advancedchat.config.FileList.FileName;
 public class ConfigManager extends FileManager{
 	
 
 	private AdvancedChat plugin;
+	@SuppressWarnings("unused")
 	private File file;
-	private String[] folderNames = new String[]{"Data","Gui","Log"};
 	private HashMap<String, YamlConfiguration> configs;
-	private boolean removeExist;
+	@SuppressWarnings("unused")
+	private FileConfiguration config , players, colorgui, playergui;
+	//private boolean removeExist;
 	
 	public ConfigManager(AdvancedChat plugin) {
 		super(plugin);
 		this.plugin = plugin;
+		this.file = null;
 	}
 	
-	/*public void load() {
-		this.removeExist = this.configs != null;
-		if(!removeExist) {
-			this.configs = new HashMap<>();
+	public void create() {
+		if(!getDataFolder().exists()) {
+			getDataFolder().mkdirs();
 		}
 		
-		for(int i = 0; i < files.getName(FileName.valueOf(configs.))) {
-			
-		}
-	}*/
-	
-	public YamlConfiguration getConfig(FileName name) {
-		return (YamlConfiguration) this.configs.get(Files.getName(name));
+		this.file = new File(getDataFolder(), "config.yml");
+		this.file = new File(getDataFolder() + getFolderData(), "players.yml");
+		this.file = new File(getDataFolder() + getFolderGui(), "color-gui.yml");
+		this.file = new File(getDataFolder() + getFolderGui(), "player-gui.yml");
+		this.file = new File(getDataFolder() + getFolderLog() , "chat-log.yml");
+		
 	}
 	
-
+	public YamlConfiguration getConfig(FileName name) {
+		return (YamlConfiguration) this.configs.get(FileList.getName(name));
+	}
 	
+	public FileConfiguration getConfig(String name) {
+		if(name.equalsIgnoreCase("config")) {
+			if(config == null) {
+				reloadConfig("config.yml");
+			}
+			return config;
+		}
+		
+		if(name.equalsIgnoreCase("config")) {
+			if(config == null) {
+				reloadConfig("players.yml");
+			}
+			return players;
+		}
+		if(name.equalsIgnoreCase("config")) {
+			if(config == null) {
+				reloadConfig("config.yml");
+			}
+			return config;
+		}
+		
+		return null;
+	}
+	
+	public void save(String filename) {
+		
+	}
+	
+	public void reloadConfig(String filename) {
+		
+	}
+	
+	public void reloadAllConfig() {
+		
+	}
+	
+	public AdvancedChat getPlugin() {
+		return this.plugin;
+	}
+	
+	public String getFolderData() {
+		return File.separator + "Data";
+	}
+	
+	public String getFolderGui() {
+		return File.separator + "Gui";
+	}
+	
+	public String getFolderLog() {
+		return File.separator + "Log";
+	}
 }
