f2ec3b37a80efcf2c10959d04a130fc39292c41f
jonagamerpro1234 <js398704@gmail.com>
2021-11-11 22:24:20 -0300

    v1.6.0-pre-20 update advancedchat maven

---
+++ AdvancedChat/Shade/src/main/java/jss/advancedchat/update/UpdateSettings.java
@@ -0,0 +1,12 @@
+package jss.advancedchat.update;
+
+
+public class UpdateSettings {
+
+    public static String[] URL_PLUGIN = new String[] {"https://www.spigotmc.org/resources/advancedchat-1-7-x-1-16-x.83889/","https://songoda.com/marketplace/product/advancedchat-chat-related.542","https://github.com/jonagamerpro1234/AdvancedChat/releases"};
+    public static int[] ID = new int[] {83889,542};
+    public static String NAME = "AdvancedChat";
+    public static String VERSION = "1.6.0";
+    public static String SPIGOT_UPDATE_API = "https://api.spigotmc.org/legacy/update.php?resource=";
+    
+}
