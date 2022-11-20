7ea28c3fbce9c2d18e409e2d116797a2d74fcd7e
SrJss <31166687+jonagamerpro1234@users.noreply.github.com>
2022-11-19 21:49:48 -0300

    Add new FileCreator

--- Bukkit/src/main/java/jss/advancedchat/AdvancedChat.java
+++ Bukkit/src/main/java/jss/advancedchat/AdvancedChat.java
@@ -1,10 +1,17 @@
 package jss.advancedchat;
 
 import jss.advancedchat.commands.CommandHandler;
+import jss.advancedchat.files.FileCreator;
+import jss.advancedchat.files.LangFile;
+import jss.advancedchat.files.utils.PreConfigLoader;
 import jss.advancedchat.utils.Utils;
 import org.bstats.bukkit.Metrics;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public final class AdvancedChat extends JavaPlugin {
 
     private static AdvancedChat instance;
@@ -12,6 +19,9 @@ public final class AdvancedChat extends JavaPlugin {
     public Metrics metrics;
     public final String name = jss.getName();
     public final String version = jss.getVersion();
+    public FileCreator fileCreator = new FileCreator(this);
+    private Map<String, LangFile> langFileMap = new HashMap<>();
+    private PreConfigLoader preConfigLoader = new PreConfigLoader();
 
     public void onLoad(){
         instance = this;
@@ -19,6 +29,12 @@ public final class AdvancedChat extends JavaPlugin {
     }
     public void onEnable() {
         metrics = new Metrics(this,8826);
+
+        fileCreator.saveDefaultConfig("test");
+        fileCreator.create("","test", false);
+        fileCreator.saveDefaultConfig("Test","test",true);
+        fileCreator.create("Test","test",true);
+
 
         //message
         Utils.sendEnable();
--- Bukkit/src/main/java/jss/advancedchat/files/FileCreator.java
+++ Bukkit/src/main/java/jss/advancedchat/files/FileCreator.java
@@ -1,64 +1,106 @@
 package jss.advancedchat.files;
 
 import jss.advancedchat.AdvancedChat;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.jetbrains.annotations.NotNull;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
 
 public class FileCreator {
 
-    private final AdvancedChat plugin = AdvancedChat.get();
+    private final AdvancedChat plugin;
     private File file;
     private YamlConfiguration config;
 
-    public void createFile(String name) {
-        this.file = new File(plugin.getDataFolder(), name + ".yml");
+    public FileCreator(AdvancedChat plugin) {
+        this.plugin = plugin;
+    }
 
-        if (!plugin.getDataFolder().exists()) {
-            //noinspection ResultOfMethodCallIgnored
-            plugin.getDataFolder().mkdirs();
+    public void create(String folder,String name, boolean inFolder) {
+        String dir;
+        if(inFolder){
+            dir = plugin.getDataFolder() + File.separator + folder;
+        }else{
+            dir = plugin.getDataFolder().getName();
         }
 
+
+        this.file = new File(dir, name + ".yml");
         if (!this.file.exists()) {
-            try {
-                //noinspection ResultOfMethodCallIgnored
-                this.file.getParentFile().mkdirs();
-                //noinspection ResultOfMethodCallIgnored
-                this.file.createNewFile();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            this.config = YamlConfiguration.loadConfiguration(file);
-        } else {
-            try {
-                throw new IOException();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            getConfig(folder,name,inFolder).options().copyDefaults(true);
+            saveConfig(folder,name,inFolder);
         }
     }
 
-    public boolean saveFile(@NotNull YamlConfiguration config, String name) {
+    public FileConfiguration getConfig(String folder, String name, boolean inFolder) {
+        if (this.config == null) {
+            reloadConfig(name);
+        }
+        file = getFile(folder,name,inFolder);
+        config = YamlConfiguration.loadConfiguration(file);
+        return this.config;
+    }
+
+    public FileConfiguration getConfig(String name) {
+        return getConfig("",name,false);
+    }
+
+
+    public void saveConfig(String folder, String name, boolean inFolder) {
         try {
-            config.save(getFile(name));
-            return true;
+            file = getFile(folder,name,inFolder);
+            this.config.save(this.file);
         } catch (IOException e) {
             e.printStackTrace();
-            return false;
         }
     }
 
-    public File getFile(String name) {
-        this.file = new File(plugin.getDataFolder(), name + ".yml");
-        return this.file;
+    public void saveConfig(String name) {
+        saveConfig("",name,false);
     }
 
-    public YamlConfiguration getConfig(String name) {
-        this.file = new File(plugin.getDataFolder(), name + ".yml");
+    public void reloadConfig(String name) {
+        if (this.config == null) {
+            this.file = new File(plugin.getDataFolder(), name + ".yml");
+        }
         this.config = YamlConfiguration.loadConfiguration(this.file);
-        return this.config;
+        Reader defaultConfigStream;
+        try {
+            //noinspection ConstantConditions
+            defaultConfigStream = new InputStreamReader(plugin.getResource(name + ".yml"), StandardCharsets.UTF_8);
+            BufferedReader in = new BufferedReader(defaultConfigStream);
+            YamlConfiguration defaultConfig = YamlConfiguration.loadConfiguration(in);
+            config.setDefaults(defaultConfig);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void saveDefaultConfig(String folder, String name, boolean inFolder) {
+        if (this.file == null) {
+            this.file = getFile(folder, name, inFolder);
+        }
+        if (!this.file.exists()) {
+            plugin.saveResource(name + ".yml", false);
+        }
+    }
+
+    public void saveDefaultConfig(String name) {
+        saveDefaultConfig("",name,false);
+    }
+
+    public File getFile(String folder, String name, boolean inFolder) {
+        String dir;
+        if(inFolder){
+            dir = plugin.getDataFolder() + File.separator + folder;
+        }else{
+            dir = plugin.getDataFolder().getName();
+        }
+        this.file = new File(this.plugin.getDataFolder(), name + ".yml");
+        return this.file;
     }
 
 }
