46565190d44e997b39e6ca67ef67049898a2d205
SrJss <31166687+jonagamerpro1234@users.noreply.github.com>
2026-07-01 22:15:36 -0400

    refactor: begin next generation architecture

---
+++ Platform/Paper/src/main/java/jss/advancedchat/paper/Paper.java
@@ -0,0 +1,16 @@
+package jss.advancedchat.paper;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Paper extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
